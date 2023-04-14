97ee15ea2b2cb97d2cfb43e6c6791aa536019f71
kosiu1231 <aroczek1@wp.pl>
2023-04-14 17:27:00 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Pages from "./pages/Pages";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="App">
             <Pages />
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,7 +3,11 @@ import AllNews from "./AllNews";
 import NewsDetails from "./NewsDetails";
 
 const Pages = () => {
-    return <div>Pages</div>;
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    );
 };
 
 export default Pages;
