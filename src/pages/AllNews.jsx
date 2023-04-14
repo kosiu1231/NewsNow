5e2ff047657570d32237670cb42584094445eccb
kosiu1231 <aroczek1@wp.pl>
2023-04-14 17:32:42 +0200

    Test

--- src/pages/AllNews.jsx
+++ src/pages/AllNews.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "../components/nav";
+import Nav from "../components/Nav";
 
 const AllNews = () => {
     return <div>AllNews</div>;
--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "../components/nav";
+import Nav from "../components/Nav";
 
 const NewsDetails = () => {
     return <div>NewsDetails</div>;
