1507e9321f2d1094dabf79a9ef01722823bdc087
kosiu1231 <aroczek1@wp.pl>
2023-04-15 15:16:57 +0200

    fixed css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,27 +4,29 @@ import "../App.css";
 const Nav = () => {
     return (
         <div className="navContainer">
-            <div className="header">
-                <img className="logo" src="/logo-globe.png" alt="logo" />
-                <h1 className="headerName">
-                    <strong>News</strong>Now
-                </h1>
-            </div>
-            <div className="search">searchbox</div>
-            <div className="categories">
-                <h2>Categories</h2>
-                <ul>
-                    <li>Element 1</li>
-                    <li>Element 2</li>
-                    <li>Element 3</li>
-                </ul>
-            </div>
-            <footer>
-                <p>
-                    Made by <a href="https://github.com/kosiu1231">Kosiu</a>{" "}
-                    using <a href="https://newsapi.org/">NewsApi</a>
-                </p>
-            </footer>
+            <nav>
+                <div className="header">
+                    <img className="logo" src="/logo-globe.png" alt="logo" />
+                    <h1 className="headerName">
+                        <strong>News</strong>Now
+                    </h1>
+                </div>
+                <div className="search">searchbox</div>
+                <div className="categories">
+                    <h2>Categories</h2>
+                    <ul>
+                        <li>Element 1</li>
+                        <li>Element 2</li>
+                        <li>Element 3</li>
+                    </ul>
+                </div>
+                <footer>
+                    <p>
+                        Made by <a href="https://github.com/kosiu1231">Kosiu</a>{" "}
+                        using <a href="https://newsapi.org/">NewsApi</a>
+                    </p>
+                </footer>
+            </nav>
         </div>
     );
 };
